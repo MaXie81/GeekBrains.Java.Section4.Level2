02f0ff434d41e6095a0bae6dd3646ae28b8bd225
KrakenUser <KrakenUser@yandex.ru>
2022-08-07 18:53:24 +0300

    Пункт 1. Реалилзация.

---
+++ src/main/java/Test.java
@@ -0,0 +1,5 @@
+public class Test {
+    public static void main(String[] args) {
+        Person person = new Person.Builder().setFirstName("Иванов").setMiddleName("Иван").setLastName("Иванович").build();
+    }
+}
