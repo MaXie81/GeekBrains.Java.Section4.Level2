e885eec8ebee08726284ed5b3b4a485a58980d1d
KrakenUser <KrakenUser@yandex.ru>
2022-08-21 13:00:27 +0300

    Создан класс сущность Student. Класс для тестирования. Протестировано создание соответстующих таблиц в БД.

---
+++ src/main/java/Student.java
@@ -0,0 +1,57 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name = "STUDENT")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "MARK")
+    private String mark;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public Student() {
+    }
+
+    public Student(String name, String mark) {
+        this.name = name;
+        this.mark = mark;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", mark='" + mark + '\'' +
+                '}';
+    }
+}
