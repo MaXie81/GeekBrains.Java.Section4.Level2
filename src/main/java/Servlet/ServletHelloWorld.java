2be099b45c068163bde9bd1eecd2a4f84b7fd49d
KrakenUser <KrakenUser@yandex.ru>
2022-08-25 12:59:33 +0300

    Рабочий вариант.

---
+++ src/main/java/Servlet/ServletHelloWorld.java
@@ -0,0 +1,34 @@
+package Servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ServletHelloWorld", urlPatterns = "HelloWorld")
+public class ServletHelloWorld extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        //
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+
+        String html =
+                "<html>" +
+                "<head>" +
+                "<title>ServletHelloWorld</title>" +
+                "</head>" +
+                "<body>" +
+                "<h3>Hello World!</h3>" +
+                "</body>" +
+                "</html>";
+
+        resp.getWriter().println(html);
+    }
+}
