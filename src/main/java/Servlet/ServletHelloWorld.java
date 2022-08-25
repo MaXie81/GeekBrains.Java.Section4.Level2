e3b4bbc40986f037bb6cfac0576c09463971ff8d
KrakenUser <KrakenUser@yandex.ru>
2022-08-25 14:08:58 +0300

    Финальная версия 2.

--- src/main/java/Servlet/ServletHelloWorld.java
+++ src/main/java/Servlet/ServletHelloWorld.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ServletHelloWorld", urlPatterns = "/ServletHelloWorld")
+@WebServlet(name = "ServletHelloWorld", urlPatterns = "")
 public class ServletHelloWorld extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
